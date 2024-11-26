affc17288626dbe9173ef58a37516a0b77a91e89
mcichosz313 <m.cichosz.313@studms.ug.edu.pl>
2024-11-26 22:01:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,120 +9,86 @@ def f(x,dy):
     #return numpy.float64(dy[1])**2-numpy.float64(dy[0])
 #dy[i] is the (i)th derivative. dy[0] is the value of y.
 
-print ("Please enter the order of the equation.")
-order=int(input())
-#order=int(5)
+print ("Do you want to load the input from a file in the program directory? (Y/N)")
+input_from_file=input()
 
-print ("Please enter the number of steps the program has to calculate.")
-steps=int(input())
+if input_from_file=="Y":
+    f=open("input.txt", "r")
+    order = int(f.readline())
+    steps = int(f.readline())
+    h = float(f.readline())
+    x = [0] * (steps + 1)
+    x[0] = float(f.readline())
+    derivatives = numpy.zeros([order + 1, steps + 1])
+    for i in range(0, order):
+        if i > 3:
+            derivatives[i, 0] =float(f.readline())
+            print(derivatives[i, 0])
+        elif i == 0:
+            derivatives[i, 0] =float(f.readline())
+            print(derivatives[i, 0])
+        elif i == 1:
+            derivatives[i, 0] =float(f.readline())
+            print(derivatives[i, 0])
+        elif i == 2:
+            derivatives[i, 0] =float(f.readline())
+            print(derivatives[i, 0])
+        elif i == 3:
+            derivatives[i, 0] =float(f.readline())
+            print(derivatives[i, 0])
+    #f.close()
+else:
+    print ("Please enter the order of the equation.")
+    order=int(input())
+    #order=int(5)
 
-print ("Please enter the step between x values.")
-h=float(input())
+    print ("Please enter the number of steps the program has to calculate.")
+    print ("WARNING: The program may crash for too many steps if the solution diverges to infinity.")
+    steps=int(input())
 
-#The program uses initial conditions for the same x for all derivatives of the function y.
-x=[0]*(steps+1)
-print ("Please enter the initial value of the variable x.")
-x[0]=float(input())
+    print ("Please enter the step between x values.")
+    h=float(input())
 
-#y=[0]*(steps+1)
-#print ("Please enter the initial value of the function y.")
-#y[0]=int(input())
+    #The program uses initial conditions for the same x for all derivatives of the function y.
+    x=[0]*(steps+1)
+    print ("Please enter the initial value of the variable x.")
+    x[0]=float(input())
 
-#derivatives=[[0]*order+1]*(steps+1)
-derivatives=numpy.zeros([order+1, steps+1])
+    #y=[0]*(steps+1)
+    #print ("Please enter the initial value of the function y.")
+    #y[0]=int(input())
 
-for i in range (0, order):
-    if i>3:
-        print("Please enter the initial value of", i, "-th derivative.")
-        derivatives[i,0]=float(input())
-    elif i==0:
-        print ("Please enter the initial value of the function y.")
-        derivatives[i,0]=float(input())
-    elif i==1:
-        print("Please enter the initial value of first derivative.")
-        derivatives[i,0]=float(input())
-    elif i==2:
-        print("Please enter the initial value of second derivative.")
-        derivatives[i,0]=float(input())
-    elif i==3:
-        print("Please enter the initial value of third derivative.")
-        derivatives[i,0]=float(input())
+    #derivatives=[[0]*order+1]*(steps+1)
+    derivatives=numpy.zeros([order+1, steps+1])
+
+    for i in range (0, order):
+        if i>3:
+            print("Please enter the initial value of", i, "-th derivative.")
+            derivatives[i,0]=float(input())
+        elif i==0:
+            print ("Please enter the initial value of the function y.")
+            derivatives[i,0]=float(input())
+        elif i==1:
+            print("Please enter the initial value of first derivative.")
+            derivatives[i,0]=float(input())
+        elif i==2:
+            print("Please enter the initial value of second derivative.")
+            derivatives[i,0]=float(input())
+        elif i==3:
+            print("Please enter the initial value of third derivative.")
+            derivatives[i,0]=float(input())
 
 f0=[0]*(order+1)
 f1=[0]*(order+1)
 f2=[0]*(order+1)
 f3=[0]*(order+1)
 fh=[0]*(order+1)
-#for i in range (1,steps+1):
-#    print (derivatives[:,i-1])
-'''
-#for i in range (1,steps+1):
-    #dy=derivatives[:,i-1]
-for i in range (1,steps+1):
-    for j in range (0,order+1):
-        if j==order:
-            #f0[j]=f(x[i-1],y[i-1],derivatives[0][i-1],derivatives[1][i-1],derivatives[2][i-1],derivatives[3][i-1])
-            #f0=f(x[i-1],y[i-1],derivatives[:,i-1])
-            f0[j] = f(x[i-1], derivatives[:,i-1])
-        else:
-            f0[j]=derivatives[j+1,i-1]*h
-    for j in range (0,order+1):
-        for k in range(0, order + 1):
-            fh[k] = f0[k]/ 2
-        if j==order:
-            #f1[j]=f(x[i-1]+h/2,y[i-1]+h/2*f0[0],derivatives[0][i-1]+h/2*f0[1],derivatives[1][i-1]+h/2*f0[2],derivatives[2][i-1]+h/2*f0[3],derivatives[3][i-1]+h/2*f0[4])
-            #f1[j] = f(x[i-1]+h/2,y[i-1]+h/2*f0[0],(derivatives[:,i-1]+))
-            f1[j] = f(x[i-1]+h/2, derivatives[:,i-1]+fh)
-        elif j==order-1:
-            f1[j] = f1[j+1]*h
-        else:
-            f1[j]=(derivatives[j+1][i-1]+f0[j+1]/2)*h
-            #f1[j]=f0[j]
-    for j in range (0,order+1):
-        for k in range(0, order + 1):
-            fh[k] = f1[k]/ 2
-        if j==order:
-            #f2[j]=f(x[i-1]+h/2,y[i-1]+h/2*f1[0],derivatives[0][i-1]+h/2*f1[1],derivatives[1][i-1]+h/2*f1[2],derivatives[2][i-1]+h/2*f1[3],derivatives[3][i-1]+h/2*f1[4])
-            f2[j] = f(x[i-1]+h/2, derivatives[:,i-1]+fh)
-        elif j == order - 1:
-            f2[j] = f2[j + 1] * h
-        else:
-            f2[j]=(derivatives[j+1][i-1]+f1[j+1]/2)*h
-            #f2[j] = f1[j]
-    for j in range (0,order+1):
-        for k in range(0, order + 1):
-            fh[k] = f2[k]
-        if j==order:
-            #f3[j]=f(x[i-1]+h,y[i-1]+h*f2[0],derivatives[0][i-1]+h*f2[1],derivatives[1][i-1]+h*f2[2],derivatives[2][i-1]+h*f2[3],derivatives[3][i-1]+h*f2[4])
-            f3[j] = f(x[i-1]+h, derivatives[:,i-1]+fh)
-        elif j == order - 1:
-            f3[j] = f3[j + 1] * h
-        else:
-            f3[j]=(derivatives[j+1][i-1]+f2[j+1])*h
-            #f3[j] = f2[j]
-    x[i] = x[i - 1] + h
-    #f1[order] = f0[order]
-    #f2[order] = f0[order]
-    #f3[order] = f0[order]
-    for j in range(0,order+1):
-        if j == order:
-            derivatives[j][i]=derivatives[j][i-1]+(f0[j]+f1[j]*2+f2[j]*2+f3[j])/6
-        else:
-            derivatives[j][i]=derivatives[j][i-1]+(f0[j]+f1[j]*2+f2[j]*2+f3[j])/6
-'''
+for i in range (0,10):
+    print (type(derivatives[:,0]))
 
-'''for i in range (1,steps+1):
-    x[i]=x[i-1]+h
-    #y[i]=y[i-1]+h/6*(f0[0]+f1[0]*2+f2[0]*2+f3[0])
-    derivatives[order][i]=f(x[i-1],derivatives[:,i-1])
-    #derivatives[order][i]=1
-    for j in range(0, order):
-        derivatives[j][i]=derivatives[j][i-1]+derivatives[j+1][i-1]*h
-    #for j in range(0,order+1):
-        #derivatives[j][i]=derivatives[j][i-1]+(f0[j]*h+f1[j]*2*h+f2[j]*2*h+f3[j]*h)/6
-'''
+
 derivatives[order][0]=0
-derivatives[order][0]=f(x[0],derivatives[:,i-1])
+derivatives[order][0]=f(x[0],derivatives[:,0])
 
 for i in range (1,steps+1):
     x[i]=x[i-1]+h
